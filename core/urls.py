65c85b8e1614bf10f6f34555d776047528abf4b4
tanmay1413 <tanmaykulkarni1413@gmail.com>
2025-07-02 18:11:32 +0530

    admin page setup

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,9 @@ from .views import *
 urlpatterns = [
   path('', index_page, name="index"),
   path('dashboard/', dashboard, name='dashboard'),
+  path('upload-files/', upload_files, name='upload_files'),
+  path('modify-data/', modify_data, name="modify_data"),
+  path('manage-users/', manage_user, name='manage_users'),
+  path('client-info/',client_info, name="client_info"),
   
 ]
--- core/views.py
+++ core/views.py
@@ -14,3 +14,19 @@ def dashboard(request):
         return render(request, 'home.html')  # Admin dashboard
     else:
         return render(request, 'customer_home.html')  # Regular user dashboard
+
+@login_required
+def upload_files(request):
+    return render(request,'upload_files.html')
+
+@login_required
+def modify_data(request):
+  return render(request,'modify_data.html')
+
+@login_required
+def manage_user(request):
+   return render(request, 'manageUsers.html')
+
+@login_required
+def client_info(request):
+   return render(request, 'client_info.html')
