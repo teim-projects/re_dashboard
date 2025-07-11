3696e8e8fb1f0b6ce555b11064c644c2119f9990
teim-projects <108449533+teim-projects@users.noreply.github.com>
2025-07-11 16:12:38 +0530

    uplode

--- customer_dashboard/urls.py
+++ customer_dashboard/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path
 from .views import *
 
 urlpatterns = [
-  path('',dashboard, name="customer_dashboard")
-  
+    path('', dashboard, name="customer_dashboard"),  # Wind
+    path('solar/', solar_dashboard, name="solar_dashboard"),  # Solar
 ]
--- customer_dashboard/views.py
+++ customer_dashboard/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def dashboard(request):
-  return render(request, 'customer_home.html')
+    return render(request, 'customer_home.html')  # Wind Dashboard
+
+def solar_dashboard(request):
+    return render(request, 'solar_dashboard.html')  # Solar Dashboard
